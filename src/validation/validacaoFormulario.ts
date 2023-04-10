7614346ad6ac2453d06f871e2c014dc98e924395
Samuel Silva <samuelmec1@gmail.com>
2023-04-10 10:15:10 -0300

    validacao do formulario

---
+++ src/validation/validacaoFormulario.ts
@@ -0,0 +1,28 @@
+import * as yup from 'yup'
+
+const FormSchema = yup.object().shape({
+  nomeEmpresa: yup
+    .string()
+    .min(3, "Nome da empresa deve conter no minimo 3 caracteres")
+    .required("campo obrigatório"),
+  telefone: yup
+    .string().required("campo obrigatório"),
+  email: yup
+    .string()
+    .email("digite um email valido")
+    .required("campo obrigatório"),
+  uf: yup
+    .string().required("campo obrigatório"),
+  cidade: yup
+    .string().required("campo obrigatório"),
+  cep: yup
+    .string().required("campo obrigatório"),
+  ramoAtividade: yup
+    .string().required("campo obrigatório"),
+  detalheAtividade: yup
+    .string().required("campo obrigatório"),
+  servicosDesejados: yup
+    .string().required("campo obrigatório")
+})
+
+export default FormSchema
