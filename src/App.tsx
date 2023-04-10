e7f859a2bc5550b8bde31cba169d4838b3000278
Samuel Silva <samuelmec1@gmail.com>
2023-04-10 10:15:57 -0300

    Adicionando importacoes e adicionando component toastify no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/Themes/defaultTheme'
 import { Home } from './Pages/Home'
 import { GlobalStyle } from './styles/global'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.min.css'
 
 function App() {
 
@@ -10,6 +13,7 @@ function App() {
       <Home />
 
       <GlobalStyle />
+      <ToastContainer />
     </ThemeProvider>
   )
 }
